e86656a88cad925f25fc21922430f84d784a1022
yingeer <2973780290@qq.com>
2020-02-13 14:34:25 +0800

    写好qingcheng.goods.Brand表 对应的pojo和基本dao, service

---
+++ qingcheng_pojo/src/main/java/com/qingcheng/pojo/goods/Brand.java
@@ -0,0 +1,71 @@
+package com.qingcheng.pojo.goods;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_brand", schema="qingcheng_goods")
+public class Brand implements Serializable {
+    @Id
+    private Integer id;
+
+    private  String name;
+
+    private String image;
+
+    private String letter;
+
+    private Integer seq;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getLetter() {
+        return letter;
+    }
+
+    public void setLetter(String letter) {
+        this.letter = letter;
+    }
+
+    public Integer getSeq() {
+        return seq;
+    }
+
+    public void setSeq(Integer seq) {
+        this.seq = seq;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Brand{name: "+name+
+                " image: " + image +
+                " letter: " + image +
+                " seq: " + seq +
+                "}";
+
+    }
+}
